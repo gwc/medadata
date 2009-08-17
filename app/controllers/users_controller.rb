c3d6d95361b91a9589aa6e17ec91b38cec204c07
Gavin Walters <gavinwalters@shaw.ca>
2009-08-16 21:27:59 -0600

    worked on layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,21 +15,21 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       flash[:notice] = "Successfully created user."
-      redirect_to @user
+      redirect_to root_url
     else
       render :action => 'new'
     end
   end
   
   def edit
-    @user = User.find(params[:id])
+    @user = current_user
   end
   
   def update
-    @user = User.find(params[:id])
+    @user = current_user
     if @user.update_attributes(params[:user])
       flash[:notice] = "Successfully updated user."
-      redirect_to @user
+      redirect_to root_url
     else
       render :action => 'edit'
     end
